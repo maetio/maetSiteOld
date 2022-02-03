6f449decac6cb14158c017e2698d99a5b1327765
lockjio <jio.buenviaje@gmail.com>
2022-02-02 19:35:35 -0500

    Initialize new site

---
+++ src/components/header/header.data.js
@@ -0,0 +1,20 @@
+export default [{}];
+
+// export default [
+//   {
+//     path: 'home',
+//     label: 'Home',
+//   },
+//   {
+//     path: 'feature',
+//     label: 'Features',
+//   },
+//   {
+//     path: 'pricing',
+//     label: 'Pricing',
+//   },
+//   {
+//     path: 'testimonial',
+//     label: 'Testimonial',
+//   },
+// ];
